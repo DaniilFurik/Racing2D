a308c901512f9054ed470544905962fe8acd2451
DaniilFurik <danikfurik@gmail.com>
2024-11-25 19:20:34 +0300

    init commit

---
+++ Racing2D/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Racing2D
+//
+//  Created by Даниил on 25.11.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemGreen
+    }
+}
